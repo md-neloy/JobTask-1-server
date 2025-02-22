c973fce888353d7951be4686ae8a7c4c01b6391e
mhneloy <mhneloy708@gmail.com>
2025-02-22 19:29:55 +0600

    create some api for the job task

--- index.js
+++ index.js
@@ -33,9 +33,76 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    const tasks = client.db("JobTask");
+    const candidatesCollection = tasks.collection("candidates");
+    const todoCollection = tasks.collection("todo");
+    const progressCollection = tasks.collection("progress");
+    const doneCollection = tasks.collection("done");
+    const userCollection = tasks.collection("user");
+
+    app.post("/tasks", async (req, res) => {
+      try {
+        const task = req.body;
+        const alltask = await candidatesCollection.insertOne(task);
+        if (task.Status === "To-do") {
+          console.log(task);
+          const result = await todoCollection.insertOne(task);
+          res.send(result);
+        } else if (task.Status === "In Progress") {
+          const result = await progressCollection.insertOne(task);
+          res.send(result);
+        } else {
+          const result = await doneCollection.insertOne(task);
+          res.send(result);
+        }
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+    });
+    app.post("/users", async (req, res) => {
+      try {
+        const user = req.body;
+        const result = userCollection.insertOne(user);
+        res.send(result);
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+    });
+
+    app.get("/tasks", async (req, res) => {
+      try {
+        const tasks = await candidatesCollection.find().toArray();
+        res.json(tasks);
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+    });
+
+    app.put("/tasks/:id", async (req, res) => {
+      try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+          new: true,
+        });
+        io.emit("taskUpdated", task);
+        res.json(task);
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+    });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      try {
+        await Task.findByIdAndDelete(req.params.id);
+        io.emit("taskDeleted", req.params.id);
+        res.status(204).send();
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
